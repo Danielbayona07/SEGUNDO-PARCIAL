8a06077835355cac0b0bfc1811af976216443ffb
Xt-Andrey <239253321+Danielbayona07@users.noreply.github.com>
2025-11-11 18:31:15 -0500

    git commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
-
-// Importar Bootstrap
+// Importar el CSS y JS de Bootstrap 5.3
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
+import 'bootstrap' 
 
-// Importar estilos personalizados
+// Importar estilos personalizados de la marca
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import App from './App.vue'
+import router from './router' 
+
+const app = createApp(App)
+
+app.use(router) 
+
+app.mount('#app')
